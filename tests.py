9ff34661e9b345b25e2956e0d68cf6e908b00cf8
Maelle <mbeugniez@kpmg.fr>
2025-02-02 23:39:44 +0100

    updzate

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 # tests/test_data_preprocessing.py
-import pytest
 import pandas as pd
 import joblib
 from data_preprocessing import preprocess_data
@@ -15,9 +14,9 @@ def test_preprocess_data():
         "SibSp": [0, 1],
         "Parch": [0, 2]
     })
-   
+
     processed_data = preprocess_data(data)
-   
+
     assert "Sex_male" in processed_data.columns
     assert "Sex_female" in processed_data.columns
     assert processed_data.shape[1] == 4  
@@ -33,9 +32,9 @@ def test_train_model():
         "SibSp": [0, 1]
     })
     y = pd.Series([1, 0])
-   
+
     model = train_model(X, y)
-   
+
     assert model is not None
     assert hasattr(model, "predict")
 
@@ -46,19 +45,19 @@ def test_predict(tmp_path):
     et génère un fichier CSV."""
     model_path = tmp_path / "test_model.pkl"
     output_path = tmp_path / "test_predictions.csv"
-   
+
     X_test = pd.DataFrame({
         "Pclass": [1, 3],
         "Sex_male": [1, 0],
         "Sex_female": [0, 1],
         "SibSp": [0, 1]
     })
-   
+
     model = joblib.load("model.pkl")
     joblib.dump(model, model_path)
-   
+
     predict(model_path, X_test, output_path)
-   
+
     assert output_path.exists()
     df_output = pd.read_csv(output_path)
     assert "Survived" in df_output.columns
