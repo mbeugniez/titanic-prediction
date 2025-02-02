fd371b90a05ce52a746c67159f4e26ae97e972c2
pgassai <precy.gassai@gmail.com>
2025-02-03 00:28:16 +0100

    Maj test

--- tests/test_e.py
+++ tests/test_e.py
@@ -3,12 +3,9 @@ import os
 import pandas as pd
 import joblib
 
-from data_preprocessing import preprocess_data
-from model_training import train_model
-from model_evaluation import predict
-
-sys.path.append(os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', 'main')))
+from src.data_preprocessing import preprocess_data
+from src.model_training import train_model
+from src.model_evaluation import predict
 
 
 def test_preprocess_data():
