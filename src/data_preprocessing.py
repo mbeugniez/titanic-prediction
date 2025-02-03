f5e3e287e6873cf41f85eb9ddec05f1d4a1ba0d2
Maelle <mbeugniez@kpmg.fr>
2025-02-03 13:01:55 +0100

    ipdate

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
+
 def load_data(train_path, test_path):
     """Charge les fichiers CSV contenant les données Titanic."""
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
     return train_data, test_data
 
-
 def preprocess_data(data):
     """Effectue le prétraitement des données en
      encodant les variables catégorielles."""
     features = ["Pclass", "Sex", "SibSp", "Parch"]
     return pd.get_dummies(data[features], drop_first=False)
-
-
 
 if __name__ == "__main__":
     train_data, test_data = load_data("train.csv", "test.csv")
